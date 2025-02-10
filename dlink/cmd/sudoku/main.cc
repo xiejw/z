924b78cbb91046093c2697127e9d07dbc22b4258
xiejw <xiejw.io+dt@gmail.com>
2025-02-10 14:36:11 -0800

    [dlink] Refactor the code.

--- dlink/cmd/sudoku/main.cc
+++ dlink/cmd/sudoku/main.cc
@@ -15,8 +15,12 @@
 #define PID 1
 #endif
 
+// === --- Predefined problems --------------------------------------------- ===
+//
 // clang-format off
 static const int PROLBEMS[][SIZE * SIZE] = {
+    // Problem ID: 0
+    //
     // From The Art of Computer Programming, Vol 4, Dancing Links.
     {
         0, 0, 3,  0, 1, 0,  0, 0, 0,
@@ -32,6 +36,8 @@ static const int PROLBEMS[][SIZE * SIZE] = {
         3, 2, 0,  0, 0, 7,  9, 5, 0,
     },
 
+    // Problem ID: 1
+    //
     // The World's Hardest Sudoku.
     //
     // https://gizmodo.com/can-you-solve-the-10-hardest-logic-puzzles-ever-created-1064112665
@@ -51,111 +57,76 @@ static const int PROLBEMS[][SIZE * SIZE] = {
 };
 // clang-format on
 
-// -----------------------------------------------------------------------------
-// Prototypes.
+// === --- Prototypes ------------------------------------------------------ ===
 //
-// -----------------------------------------------------------------------------
 struct Option {
         int x;
         int y;
         int k;
 };
+using Options = std::vector<Option>;
+using DLTable = eve::algos::dal::Table;
 
 static auto PrintProblem( const int *problem ) -> void;
-static auto SearchOptions( const int *problem, std::vector<Option> &options )
-    -> size_t;
+static auto FindAllOptions( const int *problem, Options &options ) -> size_t;
+static auto HideColHeaders( DLTable &t, const int *problem ) -> void;
+static auto InsertOptions( DLTable &t, const Options &options ) -> void;
+static auto PrintSolution( DLTable &t, const int *problem,
+                           std::vector<std::size_t> &sols ) -> void;
+
+/* For a digit k (1-based) in cell (i,j), generates four ids (1-based) for
+ * the following headers:
+ *
+ * - p{i,j}: Represent the cell position. All must have filled by a digit.
+ * - r{i,k}: Represent the row with digit. Each row must have all digits.
+ * - c{j,k}: Represent the column with digit. Each column must have all digits.
+ * - b{x,k}: Represent the box with digit where x = 3 * floor(i/3) + floor(j/3).
+ *           Each box must have all digits.
+ */
 static auto GetColHeadId( int i, int j, int k, size_t *p, size_t *r, size_t *c,
                           size_t *b ) -> void;
+// === --- main ------------------------------------------------------------ ===
 //
-//
-// -----------------------------------------------------------------------------
-// main.
-//
-// -----------------------------------------------------------------------------
 auto
 main( ) -> int
 {
-        // -------------------------------------------------------------
-        // Select and print problem
+        // === --- Select and print problem -------------------------------- ===
+        //
         const int *problem = PROLBEMS[PID];
         logInfo( "Problem:\n" );
         PrintProblem( problem );
 
-        // -------------------------------------------------------------
-        // Search options.
+        // === --- Search Options ------------------------------------------ ===
+        //
+        // At most 9^3 options, try all digits in all cells.
         std::vector<Option> options{ };
-        options.reserve( 9 * 9 * 9 );  // at most 9^3 options.
-        const size_t options_count = SearchOptions( problem, options );
+        options.reserve( 9 * 9 * 9 );
+        const size_t options_count = FindAllOptions( problem, options );
 
-        if ( DEBUG ) {
-                logInfo( "total options count %zu\n", options_count );
-                logInfo( "top 10 options:\n" );
-                for ( size_t i = 0; i < 10 && i < options_count; i++ ) {
-                        logInfo( "  x %d, y %d, k %d\n", options[i].x,
-                                 options[i].y, options[i].k );
-                }
-        }
-
-        // -------------------------------------------------------------
-        // Step 1: Prepare dancing links table.
+        // === --- Step 1: Prepare Dancing Links Table --------------------- ===
         //
-        // For each column head (item) and option, we need 4 columns to cover.
-        // See GetColHeadId for details. The extra one is the header.
+        // For each option, we need to cover 4 headers (aka items), i.e., cell
+        // position, row with digit column with digit, box with digit, and
+        // digit. This is documented in details in GetColHeadId for details.
 
-        eve::algos::dal::Table t{ /*n_col_heads=*/4 * 81,
-                                  /*n_options_total=*/4 * options_count };
+        DLTable t{ /*n_col_heads=*/4 * 81,
+                   /*n_options_total=*/4 * options_count };
 
+        // === --- Step 2: Hide all Column Headers Filled Already ---------- ===
         //
-        // -------------------------------------------------------------
-        // Step 2: Hide all column heads which have been filled by the problem
-        // already.
-        size_t item_ids[4];
-        for ( int x = 0; x < SIZE; x++ ) {
-                int offset = x * SIZE;
-                for ( int y = 0; y < SIZE; y++ ) {
-                        int num = problem[offset + y];
-                        if ( num == 0 ) continue;
+        // The problem has few digits placed already. The column headers
+        // corresponding to them should be uncovered (aka hidden).
+        HideColHeaders( t, problem );
 
-                        GetColHeadId( x, y, num,
-                                      /*p=*/item_ids,
-                                      /*r=*/item_ids + 1,
-                                      /*c=*/item_ids + 2,
-                                      /*b=*/item_ids + 3 );
-                        t.CoverCol( item_ids[0] );
-                        t.CoverCol( item_ids[1] );
-                        t.CoverCol( item_ids[2] );
-                        t.CoverCol( item_ids[3] );
-                }
-        }
-
+        // === --- Step 3: Append Options to the Dancing Links Table ------- ===
         //
-        // -------------------------------------------------------------
-        // Step 3: Append options to the dancing links table;
-        for ( size_t i = 0; i < options_count; i++ ) {
-                auto o = &options[i];
-                GetColHeadId( o->x, o->y, o->k, /*p=*/item_ids,
-                              /*r=*/item_ids + 1,
-                              /*c=*/item_ids + 2,
-                              /*b=*/item_ids + 3 );
-                t.AppendOption( item_ids, o );
-        }
+        InsertOptions( t, options );
 
-        // -------------------------------------------------------------
+        // === --- Step 4: Print the Solution ------------------------------ ===
+        //
         std::vector<std::size_t> sols{ };
-
         if ( t.SearchSolution( sols ) ) {
-                // Dup the problem as we are going to modify it.
-                int solution[SIZE * SIZE];
-                memcpy( solution, problem, sizeof( int ) * SIZE * SIZE );
-
-                logInfo( "Found solution:\n" );
-                size_t n = sols.size( );
-                for ( size_t i = 0; i < n; i++ ) {
-                        Option *o = (Option *)t.GetNodeData( sols[i] );
-                        solution[o->x * SIZE + o->y] = o->k;
-                }
-                PrintProblem( solution );
-
+                PrintSolution( t, problem, sols );
         } else {
                 printf( "No solution.\n" );
         }
@@ -163,13 +134,10 @@ main( ) -> int
         return 0;
 }
 
+// === --- Helper methods -------------------------------------------------- ===
 //
-// -----------------------------------------------------------------------------
-// helper methods.
-//
-// -----------------------------------------------------------------------------
 
-// print the Sudoku Problem on screen.
+// Print the Sudoku Problem on screen.
 auto
 PrintProblem( const int *problem ) -> void
 {
@@ -195,16 +163,16 @@ PrintProblem( const int *problem ) -> void
         }
 }
 
-#define POS( x, y ) ( ( x ) * SIZE + ( y ) )
-
 // Seach all options that on (x,y) the digit k is allowed to be put there.
 //
 // The argument options must have enough capacity to hold all potential
 // options
 auto
-SearchOptions( const int *problem, std::vector<Option> &options ) -> size_t
+FindAllOptions( const int *problem, std::vector<Option> &options ) -> size_t
 {
         size_t total = 0;
+
+#define POS( x, y ) ( ( x ) * SIZE + ( y ) )
 
         for ( int x = 0; x < SIZE; x++ ) {
                 const int offset = x * SIZE;
@@ -230,7 +198,7 @@ SearchOptions( const int *problem, std::vector<Option> &options ) -> size_t
                                         }
                                 }
 
-                                // Search box. static unroll
+                                // Search box. Static unroll
                                 if ( problem[POS( box_x, box_y )] == k ||
                                      problem[POS( box_x, box_y + 1 )] == k ||
                                      problem[POS( box_x, box_y + 2 )] == k ||
@@ -254,16 +222,75 @@ SearchOptions( const int *problem, std::vector<Option> &options ) -> size_t
                         }
                 }
         }
+
+#undef POS
+
+        if ( DEBUG ) {
+                logInfo( "total options count %zu\n", total );
+                logInfo( "top 10 options:\n" );
+                for ( size_t i = 0; i < 10 && i < total; i++ ) {
+                        logInfo( "  x %d, y %d, k %d\n", options[i].x,
+                                 options[i].y, options[i].k );
+                }
+        }
         return total;
 }
 
-// For a digit k (1-based) in cell (i,j), generats four ids (1-based) for
-// the following four col heads:
-//
-// - p{i,j} // pos
-// - r{i,k} // row with digit
-// - c{j,k} // col with digit
-// - b{x,k} // box with digit where x = 3 * floor(i/3) + floor(j/3).
+auto
+HideColHeaders( DLTable &t, const int *problem ) -> void
+{
+        size_t item_ids[4];
+        for ( int x = 0; x < SIZE; x++ ) {
+                int offset = x * SIZE;
+                for ( int y = 0; y < SIZE; y++ ) {
+                        int num = problem[offset + y];
+                        if ( num == 0 ) continue;
+
+                        GetColHeadId( x, y, num,
+                                      /*p=*/item_ids,
+                                      /*r=*/item_ids + 1,
+                                      /*c=*/item_ids + 2,
+                                      /*b=*/item_ids + 3 );
+                        t.CoverCol( item_ids[0] );
+                        t.CoverCol( item_ids[1] );
+                        t.CoverCol( item_ids[2] );
+                        t.CoverCol( item_ids[3] );
+                }
+        }
+}
+
+auto
+InsertOptions( DLTable &t, const Options &options ) -> void
+{
+        size_t item_ids[4];
+        for ( auto &opt : options ) {
+                auto *o = &opt;
+                GetColHeadId( o->x, o->y, o->k,
+                              /*p=*/item_ids,
+                              /*r=*/item_ids + 1,
+                              /*c=*/item_ids + 2,
+                              /*b=*/item_ids + 3 );
+                t.AppendOption( item_ids, (void *)o );
+        }
+}
+
+auto
+PrintSolution( DLTable &t, const int *problem, std::vector<std::size_t> &sols )
+    -> void
+{
+        // Duplicate the problem as we are going to modify it.
+        int solution[SIZE * SIZE];
+        memcpy( solution, problem, sizeof( int ) * SIZE * SIZE );
+
+        logInfo( "Found solution:\n" );
+        size_t n = sols.size( );
+        for ( size_t i = 0; i < n; i++ ) {
+                Option *o = (Option *)t.GetNodeData( sols[i] );
+                solution[o->x * SIZE + o->y] = o->k;
+        }
+        PrintProblem( solution );
+}
+
 auto
 GetColHeadId( int i, int j, int k, size_t *p, size_t *r, size_t *c, size_t *b )
     -> void
