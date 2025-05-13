7e0703d31fc0c32ddfe7d75005d92b3311a73419
Jianwei Xie <xiejw0217+db@gmail.com>
2025-05-13 16:17:36 -0700

    [llm.c] Added tok load.

---
+++ llm.c/main.c
@@ -0,0 +1,91 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+/* === Configuration -------------------------------------------------------- */
+#ifndef TOK_FILE
+#error TOK_FILE should be passed via "-DTOK_FILE"
+#endif
+
+#define TOK_READ_BUF_SIZE 4096
+
+#define PANIC( msg )                 \
+        do {                         \
+                printf( "panic\n" ); \
+                printf( msg );       \
+                exit( -1 );          \
+        } while ( 0 )
+
+/* === Tokenizer ---------------------------------------------------------------
+ *
+ * File to read
+ * - Load tok file
+ *   https://github.com/openai/tiktoken/blob/00813b3f987a083ee9f631620d0271b0169da58b/tiktoken/load.py#L146-L158
+ */
+
+
+/* terminate 0 and '\n' are excluded. */
+void
+process_line_in_tok_file( char *buf, size_t len )
+{
+        while ( len >= 1 && ( buf[len - 1] == '\0' || buf[len - 1] == '\n' ) )
+                len--;
+        if ( len == 0 ) return; /* Skip empty line */
+        printf( "%.*s\n", (int)len, buf );
+}
+
+void
+read_tok_file( void )
+{
+        char   buf[TOK_READ_BUF_SIZE];
+        char   line[TOK_READ_BUF_SIZE];
+        size_t line_idx = 0;
+
+        int fd = open( TOK_FILE, O_RDONLY );
+        if ( fd == -1 ) PANIC( "failed to open tok file" );
+
+        while ( 1 ) {
+                ssize_t c = read( fd, buf, TOK_READ_BUF_SIZE - line_idx );
+                if ( c < 0 ) PANIC( "unexpected tok file read error." );
+                if ( c == 0 ) {
+                        /* Final line in buffer */
+                        if ( line_idx > 0 )
+                                process_line_in_tok_file( line, line_idx );
+                        break;
+                }
+
+        size_t start = 0;
+        while ( 1 ) {
+                size_t end = start;
+                for ( ; buf[end] != '\n' && end < (size_t)c; end++ ) {
+                }
+                if ( end == (size_t)c ) {
+                        memcpy( line + line_idx, buf + start, end - start );
+                        line_idx += end - start;
+                        break;
+                }
+
+                /* Found a line */
+                memcpy( line + line_idx, buf + start, end - start );
+                process_line_in_tok_file( line, end - start + line_idx );
+                line_idx = 0;
+                start    = end + 1;
+        }
+}
+
+close( fd );
+}
+
+/* === Main ----------------------------------------------------------------- */
+
+int
+main( void )
+{
+        read_tok_file( );
+}
