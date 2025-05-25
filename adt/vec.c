0cd198c7efe00f6f7ccb0341dbee0d081aa5518d
Jianwei Xie <xiejw0217+main@gmail.com>
2025-05-25 10:31:45 -0700

    [adt] Started adding adt to z.

---
+++ adt/vec.c
@@ -0,0 +1,15 @@
+#ifndef ADT_VEC_H_
+#define ADT_VEC_H_
+
+/* === --- Test Code -------------------------------------------------------- */
+#ifdef ADT_TEST_H_
+#include <stdio.h>
+
+int
+main( void )
+{
+        printf( "Test passed.\n" );
+}
+
+#endif /* ADT_TEST_H_ */
+#endif /* ADT_VEC_H_ */
