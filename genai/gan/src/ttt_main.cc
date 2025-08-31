0d21623e9365673a4c156d8d2ea4e86cdcfffc3b
xiejw.io+m4max@gmail.com <xiejw.io+m4max@gmail.com>
2025-08-31 09:59:08 -0700

    [gan] added sampler.

--- genai/gan/src/ttt_main.cc
+++ genai/gan/src/ttt_main.cc
@@ -19,15 +19,18 @@ class GameState {
         char board[kGameStateCount];  // Can be "." (empty) or "X", "O".
 
       public:
-        using Player                        = char;
-        constexpr static Player PlayerNA    = '.';
-        constexpr static Player PlayerBlack = 'X';
-        constexpr static Player PlayerWhite = 'O';
+        using Symbol                  = char;
+        constexpr static Symbol SymNA = '.';
+        constexpr static Symbol SymBK = 'X';
+        constexpr static Symbol SymWT = 'O';
 
       public:
         GameState( );
 
       public:
+        // Return the board symbol at position i.
+        auto get_board_symbol( int i ) -> Symbol { return board[i]; }
+
         // Show board on screen in ASCII "art".
         auto display_board( ) -> void;
 
@@ -36,19 +39,19 @@ class GameState {
         // Instead of one-hot encoding, we can represent N different categories
         // as different bit patterns. In this specific case it's trivial:
         //
-        // 00 = PlayerNA (empty)
-        // 10 = PlayerBlack (X)
-        // 01 = PlayerWhite (O)
+        // 00 = SymNA (empty)
+        // 10 = SymBK (X)
+        // 01 = SymWT (O)
         //
         // Two inputs per symbol instead of 3 in this case, but in the general
         // case this reduces the input dimensionality A LOT.
-        auto convert_board_to_inputs( std::span<f32> &inputs ) const -> void;
+        auto convert_board_to_inputs( std::span<f32> inputs ) const -> void;
 
         /* Check if the game is over (win or tie).
          * Return None if game is not over
-         * Return PlayerNA if tie. PlayerBlack or PlayerWhite if winner exists.
+         * Return SymNA if tie. SymBK or SymWT if winner exists.
          */
-        auto is_game_over( ) -> std::optional<Player>;
+        auto is_game_over( ) -> std::optional<Symbol>;
 };
 
 GameState::GameState( ) { memset( this->board, '.', kGameStateCount ); }
@@ -69,8 +72,9 @@ GameState::display_board( ) -> void
         }
         std::print( "\n" );
 }
+
 auto
-GameState::convert_board_to_inputs( std::span<f32> &inputs ) const -> void
+GameState::convert_board_to_inputs( std::span<f32> inputs ) const -> void
 {
         if ( inputs.size( ) < kGameStateCount * 2 ) {
                 PANIC( "inputs are not big enough: expect: {} got: {}",
@@ -78,26 +82,27 @@ GameState::convert_board_to_inputs( std::span<f32> &inputs ) const -> void
         }
 
         f32 *data = inputs.data( );
-        for ( int i = 0; i < 9; i++ ) {
-                if ( this->board[i] == PlayerNA ) {
+        for ( int i = 0; i < kGameStateCount; i++ ) {
+                if ( this->board[i] == SymNA ) {
                         data[i * 2]     = 0;
                         data[i * 2 + 1] = 0;
-                } else if ( this->board[i] == PlayerBlack ) {
+                } else if ( this->board[i] == SymBK ) {
                         data[i * 2]     = 1;
                         data[i * 2 + 1] = 0;
                 } else {
-                        assert( this->board[i] == PlayerWhite );
+                        assert( this->board[i] == SymWT );
                         data[i * 2]     = 0;
                         data[i * 2 + 1] = 1;
                 }
         }
 }
+
 auto
-GameState::is_game_over( ) -> std::optional<Player>
+GameState::is_game_over( ) -> std::optional<Symbol>
 {
         // Check rows.
         for ( int i = 0; i < 3; i++ ) {
-                if ( this->board[i * 3] != PlayerNA &&
+                if ( this->board[i * 3] != SymNA &&
                      this->board[i * 3] == this->board[i * 3 + 1] &&
                      this->board[i * 3 + 1] == this->board[i * 3 + 2] ) {
                         return this->board[i * 3];
@@ -106,7 +111,7 @@ GameState::is_game_over( ) -> std::optional<Player>
 
         // Check columns.
         for ( int i = 0; i < 3; i++ ) {
-                if ( this->board[i] != PlayerNA &&
+                if ( this->board[i] != SymNA &&
                      this->board[i] == this->board[i + 3] &&
                      this->board[i + 3] == this->board[i + 6] ) {
                         return this->board[i];
@@ -114,22 +119,22 @@ GameState::is_game_over( ) -> std::optional<Player>
         }
 
         // Check diagonals.
-        if ( this->board[0] != PlayerNA && this->board[0] == this->board[4] &&
+        if ( this->board[0] != SymNA && this->board[0] == this->board[4] &&
              this->board[4] == this->board[8] ) {
                 return this->board[0];
         }
-        if ( this->board[2] != PlayerNA && this->board[2] == this->board[4] &&
+        if ( this->board[2] != SymNA && this->board[2] == this->board[4] &&
              this->board[4] == this->board[6] ) {
                 return this->board[2];
         }
 
         // Check for tie (no free tiles left).
         int empty_tiles = 0;
-        for ( int i = 0; i < 9; i++ ) {
-                if ( this->board[i] == PlayerNA ) empty_tiles++;
+        for ( int i = 0; i < kGameStateCount; i++ ) {
+                if ( this->board[i] == SymNA ) empty_tiles++;
         }
         if ( empty_tiles == 0 ) {
-                return PlayerNA;  // Tie
+                return SymNA;  // Tie
         }
 
         return std::nullopt;  // Game continues.
@@ -280,6 +285,40 @@ NeuralNetwork<IN, OUT, HIDDEN>::forward( f32 *inputs )
 
 }  // namespace eos::gan
 
+// === Sampler ------------------------------------------------------------- ===
+
+namespace eos::gan {
+
+struct Sampler {
+      public:
+        template <std::size_t IN     = kNNInputSize,
+                  std::size_t OUT    = kNNOutputSize,
+                  std::size_t HIDDEN = kNNHiddenSize>
+        static auto get_best_move( GameState                      *state,
+                                   NeuralNetwork<IN, OUT, HIDDEN> *nn ) -> int
+        {
+                f32 inputs[IN];
+
+                state->convert_board_to_inputs( inputs );
+                nn->forward( inputs );
+
+                int   best_move       = -1;
+                float best_legal_prob = -1.0f;
+
+                for ( int i = 0; i < kGameStateCount; i++ ) {
+                        // Track best legal move.
+                        if ( state->get_board_symbol( i ) == GameState::SymNA &&
+                             ( best_move == -1 ||
+                               nn->outputs[i] > best_legal_prob ) ) {
+                                best_move       = i;
+                                best_legal_prob = nn->outputs[i];
+                        }
+                }
+                return best_move;
+        }
+};
+
+}  // namespace eos::gan
 int
 main( )
 {
@@ -291,5 +330,7 @@ main( )
 
         f32 inputs[eos::gan::kNNInputSize];
         nn.forward( inputs );
+
+        eos::gan::Sampler::get_best_move( &s, &nn );
         return 0;
 }
