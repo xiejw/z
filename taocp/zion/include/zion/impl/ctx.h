cd83cd5d4bb46d01e93e97579c88374fbee22e79
xiejw.io+m4max <xiejw.io+m4max@gmail.com>
2026-01-01 05:39:02 -0800

    [zion] moved zion in.

---
+++ taocp/zion/include/zion/impl/ctx.h
@@ -0,0 +1,10 @@
+// vim: ft=cpp
+//
+// A placeholder for future Context.
+#pragma once
+
+namespace zion {
+
+class Context {};
+
+}  // namespace zion
---
+++ taocp/zion/include/zion/zion.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "impl/ctx.h"
+#include "impl/err.h"
+#include "impl/lang.h"
+#include "impl/log.h"
